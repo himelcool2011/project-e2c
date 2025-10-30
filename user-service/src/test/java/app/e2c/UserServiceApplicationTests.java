bfdebc9ef2cea9576ff05fd1fbac36fb130446e4
mehearaz <mu.himel@gmail.com>
2025-10-30 23:58:34 +0600

    user service module added

---
+++ user-service/src/test/java/app/e2c/UserServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package app.e2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
